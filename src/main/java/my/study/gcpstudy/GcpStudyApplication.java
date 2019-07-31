819901f80bfc1dee6851fa6cc9cb4d1d84285c7c
vxb97 <vitoreduardodeoliveira.bittencourt@equifax.com>
2019-07-31 11:23:54 +0100

    Initial Commit

---
+++ src/main/java/my/study/gcpstudy/GcpStudyApplication.java
@@ -0,0 +1,13 @@
+package my.study.gcpstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcpStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcpStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/my/study/gcpstudy/GcpStudyApplicationTests.java
@@ -0,0 +1,16 @@
+package my.study.gcpstudy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GcpStudyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
